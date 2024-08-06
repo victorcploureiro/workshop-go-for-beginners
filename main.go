ad8058bf764738fb40c6f567414568a754d5c9b6
Victor Capolo <victorcploureiro@gmail.com>
2024-08-06 20:49:43 -0300

    treating the errors

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
-import "fmt"
-import "os"
-import "encoding/csv"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
-func main() {
-	file, err := os.Open("testdata/users_001.csv")
+func ParseCsvFile(filePath string) ([][]string, error) {
+	file, err := os.Open(filePath)
+	defer file.Close()
 	if err != nil {
-		return
+		return nil, fmt.Errorf("error while opening csv file: %w", err)
 	}
 
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
-		return
+		return nil, fmt.Errorf("error reading csv file: %w", err)
 	}
 
+	return records, nil
+}
+
+func main() {
+	records, err := ParseCsvFile("testdata/users_001.csv")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 	fmt.Println(records)
-	file.Close()
 }
