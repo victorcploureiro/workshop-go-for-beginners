c25d69fa920d099a7bfe65898163bc29226e9af0
Victor Capolo <victorcploureiro@gmail.com>
2024-08-06 20:38:05 -0300

    not os, but csv

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 import "os"
+import "encoding/csv"
 
 func main() {
-	file, err := os.ReadFile("testdata/users_001.csv")
+	file, err := os.Open("testdata/users_001.csv")
 	if err != nil {
 		return
 	}
 
-	fmt.Println(string(file))
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return
+	}
+
+	fmt.Println(records)
+	file.Close()
 }
