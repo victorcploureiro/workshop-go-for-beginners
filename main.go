b46cd7f02ae2c08cae5de067e7522fe77efff695
Victor Capolo <victorcploureiro@gmail.com>
2024-08-06 21:27:19 -0300

    adding flags

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math/rand"
 )
 
+func SelectWinners(records, [][]string, nWinners int) [][]string {
+	rand.Shuffle(len(records), func (i, j int) {
+		records[i], records[j] = records[j], records[i]
+	})
+
+	return records[:nWinners]
+}
+
 func main() {
+	userCsvFile := flag.String("file", "testdata/users_001.csv", "Path to users file")
+	nWinners := flag.Int("winners", 6, "Number of winners to select")
 	records, err := ParseCsvFile("testdata/users_001.csv")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	fmt.Println(records)
+
+	winners :=SelectWinners(records, 3)
+	for i, winner := range winners {
+		fmt.Println("Ganhador: ", i, " ", )
+	}
 }
